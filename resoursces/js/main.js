0869636f641a3b61b9647567bfd91279a7cc559e
bpinazmul18 <bpinazmul18@gmail.com>
2020-05-23 17:14:31 +0600

    responsive site 1.0

--- resoursces/js/main.js
+++ resoursces/js/main.js
@@ -16,3 +16,11 @@ $(document).ready(function(){
 
     var mixer = mixitup('.container');
 })
+
+function openNav(){
+    document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav(){
+    document.getElementById("myNav").style.width = "0%";
+}
